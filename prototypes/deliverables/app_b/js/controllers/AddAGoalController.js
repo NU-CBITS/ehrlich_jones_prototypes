34c6b5fd542e40a1875441e7f036a5d7f2224464
Caraway <m-begale@northwestern.edu>
2016-07-25 05:04:02 -0500

    updated goal tracking for multiple pages

--- prototypes/deliverables/app_b/js/controllers/AddAGoalController.js
+++ prototypes/deliverables/app_b/js/controllers/AddAGoalController.js
@@ -3,6 +3,20 @@
 
   function AddAGoalController() {
 
+    this.pagesVisible = [true,false,false];
+
+    this.showPage = function(pageIndex){
+
+      for (var i = 0; i < this.pagesVisible.length; i++){
+
+          this.pagesVisible[i] = false;
+
+      }
+
+      this.pagesVisible[pageIndex] = true;
+
+    }
+
     this.confirmationVisible = false;
 
     this.showConfirmation = function(){
--- prototypes/deliverables/app_b/js/controllers/TrackGoalProgressController.js
+++ prototypes/deliverables/app_b/js/controllers/TrackGoalProgressController.js
@@ -5,9 +5,18 @@
 
     this.lessons = LessonPlans;
 
-    this.goToLesson = function (lessonNumber){
-      $location.path('/lessons_current');
-    }
+    this.times = [
+      {name:'1-9 minutes'},
+      {name:'10-19 minutes'},
+      {name:'20-29 minutes'},
+      {name:'30-39 minutes'},
+      {name:'40-49 minutes'},
+      {name:'50-59 minutes'},
+      {name:'60-69 minutes'},
+      {name:'70-79 minutes'},
+      {name:'80-89 minutes'},
+      {name:'90-99 minutes'},
+    ];
   
   }
 
